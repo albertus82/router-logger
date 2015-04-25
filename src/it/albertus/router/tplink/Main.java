2bd0ffa88250a09e6f6e7cc088cad98206b194a5
Albertus82 <albertus82@gmail.com>
2015-04-25 07:53:51 +0200

    Cleanup.

--- src/it/albertus/router/tplink/Main.java
+++ src/it/albertus/router/tplink/Main.java
@@ -20,16 +20,10 @@ public class Main {
 		
 		char bt;
 		while ((bt = (char)is.read()) != -1 ) {
-//			System.out.print(bt);
 			if ( bt == ':')
 				break;
 		}
 
-
-//		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(telnetClient.getOutputStream()));
-//		writer.write("admin\r");
-//		writer.newLine();
-//		writer.flush();
 		telnetClient.getOutputStream().write('a');
 		telnetClient.getOutputStream().write('d');
 		telnetClient.getOutputStream().write('m');
@@ -37,54 +31,27 @@ public class Main {
 		telnetClient.getOutputStream().write('n');
 		telnetClient.getOutputStream().write('\n');
 		telnetClient.getOutputStream().flush();
-//		Thread.sleep(1000);
-		int i = 0;
+		
 		while ((bt = (char)is.read()) != -1 ) {
-//			System.out.print(bt);
-			
-//			System.out.println("index: " + i + " - available:" + is.available());
 			if (bt == ':')
 				break;
 		}
-//		System.out.println(is.read());
-//		System.out.println(is.read());
-//		System.out.println(is.read());
-//		writer = new OutputStreamWriter(telnetClient.getOutputStream());
+		
 		telnetClient.getOutputStream().write('a');
-		telnetClient.getOutputStream().flush();
 		telnetClient.getOutputStream().write('d');
-		telnetClient.getOutputStream().flush();
 		telnetClient.getOutputStream().write('m');
-		telnetClient.getOutputStream().flush();
 		telnetClient.getOutputStream().write('i');
-		telnetClient.getOutputStream().flush();
 		telnetClient.getOutputStream().write('n');
 		telnetClient.getOutputStream().flush();
+		Thread.sleep(70);
 		telnetClient.getOutputStream().write('\n');
-		Thread.sleep(100);
 		telnetClient.getOutputStream().flush();
-		
-//		writer.write("admin");
-//		writer.newLine();
-//		writer.flush();
 
 		while ((bt = (char)is.read()) != -1 ) {
 			System.out.print(bt);
-//			if ( (char)bt == ':')
-//				break;
+			if (bt == '#')
+				break;
 		}
-		
-		
-//		InputStreamReader isr = new InputStreamReader(telnetClient.getInputStream());
-//		BufferedReader br = new BufferedReader(isr);
-////		System.out.println(br.readLine());
-////		  IOUtils.readWrite(telnetClient.getInputStream(),telnetClient.getOutputStream(),System.in,System.out);
-//		PrintWriter outPrint=new PrintWriter(telnetClient.getOutputStream(),true);
-//		System.out.println(br.readLine());System.out.println(br.readLine());
-////		outPrint.write("admin\n");
-////		outPrint.write("admin\n");
-//		System.out.println(br.readLine());
-//		outPrint.
 
 		telnetClient.disconnect();
 	}
