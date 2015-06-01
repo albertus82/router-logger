7d0e6e017084ba20db686ef804afce359454cc2d
Albertus82 <albertus82@gmail.com>
2015-06-01 09:56:22 +0200

    Reverted Configuration.toString().

--- src/it/albertus/router/RouterLoggerConfiguration.java
+++ src/it/albertus/router/RouterLoggerConfiguration.java
@@ -2,9 +2,6 @@ package it.albertus.router;
 
 import it.albertus.util.Configuration;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 public class RouterLoggerConfiguration extends Configuration {
 
 	private static final RouterLoggerConfiguration configuration = new RouterLoggerConfiguration();
@@ -15,15 +12,6 @@ public class RouterLoggerConfiguration extends Configuration {
 
 	private RouterLoggerConfiguration() {
 		super("routerlogger.cfg");
-	}
-
-	@Override
-	public String toString() {
-		final Set<String> properties = new TreeSet<String>();
-		for (final Object key : getProperties().keySet()) {
-			properties.add((String) key + '=' + getProperties().getProperty((String) key));
-		}
-		return properties.toString();
 	}
 
 }
