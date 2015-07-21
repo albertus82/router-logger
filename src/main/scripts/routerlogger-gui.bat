93950ced9197af403d1dd674665ead1e8ca1d04d
Albertus82 <albertus82@gmail.com>
2015-07-21 22:09:31 +0200

    Reduced priority of JAVAW.

--- src/main/scripts/routerlogger-gui.bat
+++ src/main/scripts/routerlogger-gui.bat
@@ -1,1 +1,1 @@
-@start "" "%JAVA_HOME%\bin\javaw.exe" -Xms16m -Xmx128m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerGui ""
+@start /BELOWNORMAL "" "%JAVA_HOME%\bin\javaw.exe" -Xms16m -Xmx128m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerGui ""
