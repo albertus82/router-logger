b573720678e314d1f20a33cdfa9247a44ed3ce2d
Albertus82 <albertus82@gmail.com>
2015-07-25 18:56:34 +0200

    GuiConsole refactoring.

--- src/main/java/it/albertus/router/gui/GuiConsole.java
+++ src/main/java/it/albertus/router/gui/GuiConsole.java
@@ -3,8 +3,6 @@ package it.albertus.router.gui;
 import it.albertus.util.Console;
 import it.albertus.util.NewLine;
 
-import java.util.Locale;
-
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.SWTException;
@@ -65,6 +63,7 @@ public class GuiConsole extends Console {
 		System.out.print(toPrint);
 	}
 
+	@Override
 	public void print(final String value) {
 		final String toPrint;
 		if (value == null) {
@@ -93,237 +92,78 @@ public class GuiConsole extends Console {
 		}
 	}
 
-	public void print(Object value) {
-		print(String.valueOf(value));
-	}
-
-	public void print(boolean value) {
-		print(String.valueOf(value));
-	}
-
-	public void print(char value) {
-		print(String.valueOf(value));
-	}
-
-	public void print(int value) {
-		print(String.valueOf(value));
-	}
-
-	public void print(long value) {
-		print(String.valueOf(value));
-	}
-
-	public void print(float value) {
-		print(String.valueOf(value));
-	}
-
-	public void print(double value) {
-		print(String.valueOf(value));
-	}
-
+	@Override
 	public void print(char array[]) {
 		print(String.valueOf(array));
 	}
 
+	@Override
 	public void println() {
 		print(NEWLINE);
 		newLine();
 	}
 
+	@Override
 	public void println(String value) {
 		print(value);
 		print(NEWLINE);
 		newLine();
 	}
 
+	@Override
 	public void println(Object value) {
 		print(value);
 		print(NEWLINE);
 		newLine();
 	}
 
+	@Override
 	public void println(boolean value) {
 		print(value);
 		print(NEWLINE);
 		newLine();
 	}
 
+	@Override
 	public void println(char value) {
 		print(value);
 		print(NEWLINE);
 		newLine();
 	}
 
+	@Override
 	public void println(int value) {
 		print(value);
 		print(NEWLINE);
 		newLine();
 	}
 
+	@Override
 	public void println(long value) {
 		print(value);
 		print(NEWLINE);
 		newLine();
 	}
 
+	@Override
 	public void println(float value) {
 		print(value);
 		print(NEWLINE);
 		newLine();
 	}
 
+	@Override
 	public void println(double value) {
 		print(value);
 		print(NEWLINE);
 		newLine();
 	}
 
+	@Override
 	public void println(char array[]) {
 		print(array);
 		print(NEWLINE);
 		newLine();
-	}
-
-	public void format(Locale l, String format, Object... args) {
-		print(String.format(l, format, args));
-	}
-
-	public void format(String format, Object... args) {
-		print(String.format(format, args));
-	}
-
-	public void printf(Locale l, String format, Object... args) {
-		format(l, format, args);
-	}
-
-	public void printf(String format, Object... args) {
-		format(format, args);
-	}
-
-	public void print(String value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		print(value);
-	}
-
-	public void print(Object value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		print(value);
-	}
-
-	public void print(boolean value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		print(value);
-	}
-
-	public void print(char value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		print(value);
-	}
-
-	public void print(int value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		print(value);
-	}
-
-	public void print(long value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		print(value);
-	}
-
-	public void print(float value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		print(value);
-	}
-
-	public void print(double value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		print(value);
-	}
-
-	public void print(char array[], boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		print(array);
-	}
-
-	public void println(String value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		println(value);
-	}
-
-	public void println(Object value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		println(value);
-	}
-
-	public void println(boolean value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		println(value);
-	}
-
-	public void println(char value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		println(value);
-	}
-
-	public void println(int value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		println(value);
-	}
-
-	public void println(long value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		println(value);
-	}
-
-	public void println(float value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		println(value);
-	}
-
-	public void println(double value, boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		println(value);
-	}
-
-	public void println(char array[], boolean onNewLine) {
-		if (onNewLine && column != 0) {
-			println();
-		}
-		println(array);
 	}
 
 }
