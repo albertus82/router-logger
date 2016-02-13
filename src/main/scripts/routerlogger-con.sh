996ec438f0617061189afbec1eb4a3dc4dda00ce
Albertus82 <albertus82@gmail.com>
2016-02-13 15:07:16 +0100

    Updated scripts (JAVA_HOME no longer needed in Linux).

--- src/main/scripts/routerlogger-con.sh
+++ src/main/scripts/routerlogger-con.sh
@@ -1,1 +1,4 @@
-"$JAVA_HOME/bin/java" -Xms4m -Xmx8m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLoggerCon
+if [ "$JAVA_HOME" != "" ]
+then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLoggerCon
+else java -Xms4m -Xmx8m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLoggerCon
+fi
--- src/main/scripts/routerlogger-gui.sh
+++ src/main/scripts/routerlogger-gui.sh
@@ -1,1 +1,4 @@
-"$JAVA_HOME/bin/java" -Xms8m -Xmx64m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLoggerGui
+if [ "$JAVA_HOME" != "" ]
+then "$JAVA_HOME/bin/java" -Xms8m -Xmx64m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLoggerGui
+else java -Xms8m -Xmx64m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLoggerGui
+fi
