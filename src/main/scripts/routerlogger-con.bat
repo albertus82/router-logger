14356d2aaf65ea1a7e9fc77a41fdc952d141e331
Albertus82 <albertus82@gmail.com>
2016-02-13 09:03:13 +0100

    Updated scripts (JAVA_HOME no longer needed in Windows).

--- src/main/scripts/routerlogger-con.bat
+++ src/main/scripts/routerlogger-con.bat
@@ -1,1 +1,3 @@
-@"%JAVA_HOME%\bin\java.exe" -Xms4m -Xmx8m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerCon
+@ECHO OFF
+IF "%JAVA_HOME%" EQU "" java.exe -Xms4m -Xmx8m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerCon
+IF "%JAVA_HOME%" NEQ "" "%JAVA_HOME%\bin\java.exe" -Xms4m -Xmx8m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerCon
--- src/main/scripts/routerlogger-gui.bat
+++ src/main/scripts/routerlogger-gui.bat
@@ -1,1 +1,3 @@
-@START /BELOWNORMAL "" "%JAVA_HOME%\bin\javaw.exe" -Xms8m -Xmx64m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerGui ""
+@ECHO OFF
+IF "%JAVA_HOME%" EQU "" START /BELOWNORMAL "" javaw.exe -Xms8m -Xmx64m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerGui
+IF "%JAVA_HOME%" NEQ "" START /BELOWNORMAL "" "%JAVA_HOME%\bin\javaw.exe" -Xms8m -Xmx64m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerGui
