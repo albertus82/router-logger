0f7afdb695563023752c2c65bd3353026138e93e
Albertus82 <albertus82@gmail.com>
2016-02-15 15:11:39 +0100

    New batch for Windows; to be ported to Linux.

---
+++ src/main/scripts/routerlogger.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+IF "%1" == "/c" GOTO CON
+IF "%1" == "/C" GOTO CON
+IF "%1" == "/?" GOTO HLP
+IF "%1" == "" GOTO GUI
+GOTO ERR
+:CON
+IF "%JAVA_HOME%" == "" java.exe -Xms4m -Xmx8m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerCon
+IF NOT "%JAVA_HOME%" == "" "%JAVA_HOME%\bin\java.exe" -Xms4m -Xmx8m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerCon
+GOTO END
+:GUI
+IF "%JAVA_HOME%" == "" START /BELOWNORMAL "" javaw.exe -Xms8m -Xmx64m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerGui
+IF NOT "%JAVA_HOME%" == "" START /BELOWNORMAL "" "%JAVA_HOME%\bin\javaw.exe" -Xms8m -Xmx64m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerGui
+GOTO END
+:ERR
+ECHO.Invalid option - "%1".
+GOTO END
+:HLP
+ECHO.Launches RouterLogger application.
+ECHO.
+ECHO.Usage: ROUTERLOGGER [/C] [/?]
+ECHO.
+ECHO.  /C	Runs in terminal
+ECHO.  /?	Shows this help
+ECHO.
+:END
