c0d2492f1eef51b7aaf8202f12590b442e02e73a
Albertus82 <albertus82@gmail.com>
2016-02-16 16:27:14 +0100

    Batch.

--- src/main/scripts/routerlogger.bat
+++ src/main/scripts/routerlogger.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 IF "%1" == "/?" GOTO HLP
-IF NOT "%2" == "" GOTO E02
+IF NOT "%2" == "" GOTO ER2
 IF "%1" == "/c" GOTO CON
 IF "%1" == "/C" GOTO CON
 IF "%1" == "" GOTO GUI
-GOTO E01
+GOTO ER1
 :CON
 IF "%JAVA_HOME%" == "" java.exe -Xms4m -Xmx8m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerCon
 IF NOT "%JAVA_HOME%" == "" "%JAVA_HOME%\bin\java.exe" -Xms4m -Xmx8m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerCon
@@ -13,11 +13,11 @@ GOTO END
 IF "%JAVA_HOME%" == "" START /BELOWNORMAL "" javaw.exe -Xms8m -Xmx64m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerGui
 IF NOT "%JAVA_HOME%" == "" START /BELOWNORMAL "" "%JAVA_HOME%\bin\javaw.exe" -Xms8m -Xmx64m -classpath "%~dp0routerlogger.jar;%~dp0lib/*" it.albertus.router.RouterLoggerGui
 GOTO END
-:E01
+:ER1
 ECHO.RouterLogger: unrecognized option - %1
 ECHO.Try 'ROUTERLOGGER /?' for more information.
 GOTO END
-:E02
+:ER2
 ECHO.RouterLogger: too many parameters - %2
 ECHO.Try 'ROUTERLOGGER /?' for more information.
 GOTO END
