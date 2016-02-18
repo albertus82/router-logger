a403f25d69dd8cc231948e70eeb953aa3a6eba0c
Albertus82 <albertus82@gmail.com>
2016-02-18 20:02:53 +0100

    Least privilege. Removed unused arrays.

--- src/main/java/it/albertus/router/gui/GuiImages.java
+++ src/main/java/it/albertus/router/gui/GuiImages.java
@@ -14,14 +14,14 @@ import org.eclipse.swt.widgets.Display;
 public class GuiImages {
 
 	public static final Image[] ICONS_ROUTER = loadIcons("router.ico");
-	public static final Image[] ICONS_WARNING = loadIcons("warning.ico");
-	public static final Image[] ICONS_ERROR = loadIcons("error.ico");
-	public static final Image[] ICONS_LOCK = loadIcons("lock.ico");
+	private static final Image TRAY_ICON_WARNING = loadIcons("warning.ico")[0];
+	private static final Image TRAY_ICON_ERROR = loadIcons("error.ico")[0];
+	private static final Image TRAY_ICON_LOCK = loadIcons("lock.ico")[0];
 
 	public static final Image TRAY_ICON_ROUTER_OK = GuiImages.ICONS_ROUTER[1];
-	public static final Image TRAY_ICON_ROUTER_WARNING = new DecorationOverlayIcon(TRAY_ICON_ROUTER_OK, ImageDescriptor.createFromImage(GuiImages.ICONS_WARNING[0]), IDecoration.BOTTOM_RIGHT).createImage();
-	public static final Image TRAY_ICON_ROUTER_ERROR = new DecorationOverlayIcon(TRAY_ICON_ROUTER_OK, ImageDescriptor.createFromImage(GuiImages.ICONS_ERROR[0]), IDecoration.BOTTOM_RIGHT).createImage();
-	public static final Image TRAY_ICON_ROUTER_LOCK = new DecorationOverlayIcon(TRAY_ICON_ROUTER_OK, ImageDescriptor.createFromImage(GuiImages.ICONS_LOCK[0]), IDecoration.BOTTOM_RIGHT).createImage();
+	public static final Image TRAY_ICON_ROUTER_WARNING = new DecorationOverlayIcon(TRAY_ICON_ROUTER_OK, ImageDescriptor.createFromImage(GuiImages.TRAY_ICON_WARNING), IDecoration.BOTTOM_RIGHT).createImage();
+	public static final Image TRAY_ICON_ROUTER_ERROR = new DecorationOverlayIcon(TRAY_ICON_ROUTER_OK, ImageDescriptor.createFromImage(GuiImages.TRAY_ICON_ERROR), IDecoration.BOTTOM_RIGHT).createImage();
+	public static final Image TRAY_ICON_ROUTER_LOCK = new DecorationOverlayIcon(TRAY_ICON_ROUTER_OK, ImageDescriptor.createFromImage(GuiImages.TRAY_ICON_LOCK), IDecoration.BOTTOM_RIGHT).createImage();
 
 	private static Image[] loadIcons(final String fileName) {
 		final InputStream is = GuiImages.class.getResourceAsStream(fileName);
