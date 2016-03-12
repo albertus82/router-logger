ce57770edfb1e6c554efadafc9ca8e1043960653
Albertus82 <albertus82@gmail.com>
2016-03-12 15:04:35 +0100

    Disconnect: confirm.

--- src/main/java/it/albertus/router/gui/listener/DisconnectListener.java
+++ src/main/java/it/albertus/router/gui/listener/DisconnectListener.java
@@ -1,9 +1,12 @@
 package it.albertus.router.gui.listener;
 
 import it.albertus.router.gui.RouterLoggerGui;
+import it.albertus.router.resources.Resources;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.MessageBox;
 
 public class DisconnectListener extends SelectionAdapter {
 
@@ -15,8 +18,14 @@ public class DisconnectListener extends SelectionAdapter {
 
 	@Override
 	public void widgetSelected(SelectionEvent e) {
-		// TODO MessageBox YES|NO
-		gui.disconnect();
+		if (gui.canDisconnect()) {
+			final MessageBox messageBox = new MessageBox(gui.getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
+			messageBox.setMessage(Resources.get("msg.confirm.disconnect.message"));
+			messageBox.setText(Resources.get("msg.confirm.disconnect.text"));
+			if (messageBox.open() == SWT.YES && gui.canDisconnect()) {
+				gui.disconnect();
+			}
+		}
 	}
 
 }
