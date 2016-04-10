f455c2d03032790430ef1c8688badd8ad277857b
Albertus82 <albertus82@gmail.com>
2016-04-10 18:25:44 +0200

    Refactoring.

--- src/main/java/it/albertus/router/gui/preference/field/EditableComboFieldEditor.java
+++ src/main/java/it/albertus/router/gui/preference/field/EditableComboFieldEditor.java
@@ -8,11 +8,12 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Remy Chi Jian Suen <remy.suen@gmail.com> - Bug 214392 missing implementation of ComboFieldEditor.setEnabled
- *     Albertus82 (http://github.com/Albertus82) - Editable Combo (could not subclass due to some private methods)
+ *     Albertus82 (http://github.com/Albertus82) - Editable Combo (could not subclass due to some private fields)
  *******************************************************************************/
 package it.albertus.router.gui.preference.field;
 
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.FocusAdapter;
@@ -22,7 +23,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 
 public class EditableComboFieldEditor extends FieldEditor {
 
@@ -52,19 +53,18 @@ public class EditableComboFieldEditor extends FieldEditor {
 
 	@Override
 	protected void adjustForNumColumns(int numColumns) {
+		final Label label = getLabelControl();
 		if (numColumns > 1) {
-			Control control = getLabelControl();
 			int left = numColumns;
-			if (control != null) {
-				((GridData) control.getLayoutData()).horizontalSpan = 1;
+			if (label != null) {
+				((GridData) label.getLayoutData()).horizontalSpan = 1;
 				left = left - 1;
 			}
 			((GridData) fCombo.getLayoutData()).horizontalSpan = left;
 		}
 		else {
-			Control control = getLabelControl();
-			if (control != null) {
-				((GridData) control.getLayoutData()).horizontalSpan = 1;
+			if (label != null) {
+				((GridData) label.getLayoutData()).horizontalSpan = 1;
 			}
 			((GridData) fCombo.getLayoutData()).horizontalSpan = 1;
 		}
@@ -76,16 +76,12 @@ public class EditableComboFieldEditor extends FieldEditor {
 		if (numColumns > 1) {
 			comboC = numColumns - 1;
 		}
-		Control control = getLabelControl(parent);
-		GridData gd = new GridData();
-		gd.horizontalSpan = 1;
-		control.setLayoutData(gd);
-		control = getComboBoxControl(parent);
-		gd = new GridData();
-		gd.horizontalSpan = comboC;
-		gd.horizontalAlignment = GridData.FILL;
-		control.setLayoutData(gd);
-		control.setFont(parent.getFont());
+		final Label label = getLabelControl(parent);
+		GridDataFactory.swtDefaults().applyTo(label);
+
+		final Combo combo = getComboBoxControl(parent);
+		GridDataFactory.swtDefaults().span(comboC, 1).align(SWT.FILL, SWT.CENTER).grab(true, false).applyTo(combo);
+		combo.setFont(parent.getFont());
 	}
 
 	@Override
