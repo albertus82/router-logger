c2349308d2185f44864fc231d7d9bdc853ce45d2
Albertus82 <albertus82@gmail.com>
2016-04-10 13:53:39 +0200

    Preference: exception management.

--- src/main/java/it/albertus/router/gui/listener/PreferencesSelectionListener.java
+++ src/main/java/it/albertus/router/gui/listener/PreferencesSelectionListener.java
@@ -2,6 +2,7 @@ package it.albertus.router.gui.listener;
 
 import it.albertus.router.gui.RouterLoggerGui;
 import it.albertus.router.gui.preference.Preferences;
+import it.albertus.router.util.Logger;
 
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -15,8 +16,14 @@ public class PreferencesSelectionListener extends SelectionAdapter {
 	}
 
 	@Override
-	public void widgetSelected(SelectionEvent e) {
-		new Preferences(gui).open();
+	public void widgetSelected(final SelectionEvent se) {
+		final Preferences preferences = new Preferences(gui);
+		try {
+			preferences.open();
+		}
+		catch (final Exception e) {
+			Logger.getInstance().log(e);
+		}
 	}
 
 }
