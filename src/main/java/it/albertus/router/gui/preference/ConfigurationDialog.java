778b971ce7c135e9493aba21bd6685f475b36007
Albertus82 <albertus82@gmail.com>
2016-04-11 12:37:58 +0200

    JFAce Preferences window localization (TbC).

--- src/main/java/it/albertus/router/gui/preference/ConfigurationDialog.java
+++ src/main/java/it/albertus/router/gui/preference/ConfigurationDialog.java
@@ -3,8 +3,10 @@ package it.albertus.router.gui.preference;
 import it.albertus.router.gui.Images;
 import it.albertus.router.resources.Resources;
 
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.preference.PreferenceManager;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 
 public class ConfigurationDialog extends PreferenceDialog {
@@ -13,10 +15,18 @@ public class ConfigurationDialog extends PreferenceDialog {
 		super(parentShell, manager);
 	}
 
+	@Override
 	protected void configureShell(final Shell newShell) {
 		super.configureShell(newShell);
 		newShell.setText(Resources.get("lbl.preferences.title"));
 		newShell.setImages(Images.MAIN_ICONS);
+	}
+
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		super.createButtonsForButtonBar(parent);
+		getButton(IDialogConstants.OK_ID).setText(Resources.get("lbl.button.ok"));
+		getButton(IDialogConstants.CANCEL_ID).setText(Resources.get("lbl.button.cancel"));
 	}
 
 }
