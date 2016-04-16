3ded30f13715de3d637f34381b797a1f4c1b55cd
Albertus82 <albertus82@gmail.com>
2016-04-16 11:25:03 +0200

    Preferences: Reader & Writer combo class check.

--- src/main/java/it/albertus/router/gui/preference/field/ReaderComboFieldEditor.java
+++ src/main/java/it/albertus/router/gui/preference/field/ReaderComboFieldEditor.java
@@ -1,6 +1,7 @@
 package it.albertus.router.gui.preference.field;
 
 import it.albertus.router.engine.RouterLoggerEngine;
+import it.albertus.router.reader.Reader;
 import it.albertus.router.resources.Resources;
 
 import org.eclipse.swt.widgets.Composite;
@@ -9,22 +10,24 @@ public class ReaderComboFieldEditor extends ValidatedComboFieldEditor {
 
 	public ReaderComboFieldEditor(final String name, final String labelText, final String[][] entryNamesAndValues, final Composite parent) {
 		super(name, labelText, entryNamesAndValues, parent);
+		setErrorMessage(Resources.get("err.preferences.combo.class.reader.invalid"));
 	}
 
 	@Override
 	protected boolean checkState() {
 		try {
-			Class.forName(RouterLoggerEngine.getReaderClassName(getValue()));
-			return true;
+			if (Reader.class.isAssignableFrom(Class.forName(RouterLoggerEngine.getReaderClassName(getValue())))) {
+				return true;
+			}
+			else {
+				setErrorMessage(Resources.get("err.preferences.combo.class.reader.invalid"));
+				return false;
+			}
 		}
 		catch (final Throwable throwable) {
+			setErrorMessage(Resources.get("err.preferences.combo.class.reader.missing"));
 			return false;
 		}
-	}
-
-	@Override
-	public String getErrorMessage() {
-		return Resources.get("err.preferences.combo.class.reader");
 	}
 
 }
--- src/main/java/it/albertus/router/gui/preference/field/WriterComboFieldEditor.java
+++ src/main/java/it/albertus/router/gui/preference/field/WriterComboFieldEditor.java
@@ -2,6 +2,7 @@ package it.albertus.router.gui.preference.field;
 
 import it.albertus.router.engine.RouterLoggerEngine;
 import it.albertus.router.resources.Resources;
+import it.albertus.router.writer.Writer;
 
 import org.eclipse.swt.widgets.Composite;
 
@@ -9,22 +10,24 @@ public class WriterComboFieldEditor extends ValidatedComboFieldEditor {
 
 	public WriterComboFieldEditor(final String name, final String labelText, final String[][] entryNamesAndValues, final Composite parent) {
 		super(name, labelText, entryNamesAndValues, parent);
+		setErrorMessage(Resources.get("err.preferences.combo.class.writer.invalid"));
 	}
 
 	@Override
 	protected boolean checkState() {
 		try {
-			Class.forName(RouterLoggerEngine.getWriterClassName(getValue()));
-			return true;
+			if (Writer.class.isAssignableFrom(Class.forName(RouterLoggerEngine.getWriterClassName(getValue())))) {
+				return true;
+			}
+			else {
+				setErrorMessage(Resources.get("err.preferences.combo.class.writer.invalid"));
+				return false;
+			}
 		}
 		catch (final Throwable throwable) {
+			setErrorMessage(Resources.get("err.preferences.combo.class.writer.missing"));
 			return false;
 		}
-	}
-
-	@Override
-	public String getErrorMessage() {
-		return Resources.get("err.preferences.combo.class.writer");
 	}
 
 }
