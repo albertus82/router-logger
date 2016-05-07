daa808ad39d5323457be0fbc7c004e3d23bb0259
Albertus82 <albertus82@gmail.com>
2016-05-07 17:34:51 +0200

    Refactoring.

--- src/main/java/it/albertus/router/email/EmailSender.java
+++ src/main/java/it/albertus/router/email/EmailSender.java
@@ -50,85 +50,6 @@ public class EmailSender {
 		long SEND_INTERVAL_IN_MILLIS = 60000L;
 	}
 
-	protected class RouterLoggerEmail {
-
-		protected final String subject;
-		protected final String message;
-		protected final File[] attachments;
-
-		protected RouterLoggerEmail(final String subject, final String message, final File[] attachments) {
-			this.subject = subject;
-			this.message = message;
-			this.attachments = attachments;
-		}
-
-		public String getSubject() {
-			return subject;
-		}
-
-		public String getMessage() {
-			return message;
-		}
-
-		public File[] getAttachments() {
-			return attachments;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + getOuterType().hashCode();
-			result = prime * result + ((message == null) ? 0 : message.hashCode());
-			result = prime * result + ((subject == null) ? 0 : subject.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) {
-				return true;
-			}
-			if (obj == null) {
-				return false;
-			}
-			if (!(obj instanceof RouterLoggerEmail)) {
-				return false;
-			}
-			RouterLoggerEmail other = (RouterLoggerEmail) obj;
-			if (!getOuterType().equals(other.getOuterType())) {
-				return false;
-			}
-			if (message == null) {
-				if (other.message != null) {
-					return false;
-				}
-			}
-			else if (!message.equals(other.message)) {
-				return false;
-			}
-			if (subject == null) {
-				if (other.subject != null) {
-					return false;
-				}
-			}
-			else if (!subject.equals(other.subject)) {
-				return false;
-			}
-			return true;
-		}
-
-		@Override
-		public String toString() {
-			return "RouterLoggerEmail [subject=" + subject + "]";
-		}
-
-		private EmailSender getOuterType() {
-			return EmailSender.this;
-		}
-
-	}
-
 	protected class EmailRunnable implements Runnable {
 
 		protected boolean exit = false;
@@ -166,12 +87,12 @@ public class EmailSender {
 
 	protected EmailSender() {
 		emailThread = new Thread(new EmailRunnable(), "emailThread");
+		emailThread.setDaemon(true);
 	}
 
 	public void init(final Console console, final Logger logger) {
 		this.out = console;
 		this.logger = logger;
-		emailThread.setDaemon(true);
 		if (!emailThread.isAlive()) {
 			emailThread.start();
 		}
@@ -179,7 +100,6 @@ public class EmailSender {
 
 	public void reserve(final String subject, final String message, final File... attachments) {
 		final RouterLoggerEmail rle = new RouterLoggerEmail(subject, message, attachments);
-		System.out.println("Reserving " + rle);
 		queue.add(rle);
 	}
 
---
+++ src/main/java/it/albertus/router/email/RouterLoggerEmail.java
@@ -0,0 +1,74 @@
+package it.albertus.router.email;
+
+import java.io.File;
+
+public class RouterLoggerEmail {
+
+	protected final String subject;
+	protected final String message;
+	protected final File[] attachments;
+
+	protected RouterLoggerEmail(final String subject, final String message, final File[] attachments) {
+		this.subject = subject;
+		this.message = message;
+		this.attachments = attachments;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public File[] getAttachments() {
+		return attachments;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof RouterLoggerEmail)) {
+			return false;
+		}
+		RouterLoggerEmail other = (RouterLoggerEmail) obj;
+		if (message == null) {
+			if (other.message != null) {
+				return false;
+			}
+		}
+		else if (!message.equals(other.message)) {
+			return false;
+		}
+		if (subject == null) {
+			if (other.subject != null) {
+				return false;
+			}
+		}
+		else if (!subject.equals(other.subject)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "RouterLoggerEmail [subject=" + subject + "]";
+	}
+
+}
