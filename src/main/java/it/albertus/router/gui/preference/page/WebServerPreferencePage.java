8e97368e118d1f58c5a94f0f4121680ec38c6148
Albertus82 <albertus82@gmail.com>
2016-05-22 10:17:27 +0200

    Preferences - Web server: added header.

--- src/main/java/it/albertus/router/gui/preference/page/WebServerPreferencePage.java
+++ src/main/java/it/albertus/router/gui/preference/page/WebServerPreferencePage.java
@@ -1,5 +1,20 @@
 package it.albertus.router.gui.preference.page;
 
+import it.albertus.router.gui.TextFormatter;
+import it.albertus.router.resources.Resources;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+
 public class WebServerPreferencePage extends BasePreferencePage {
+
+	@Override
+	protected Control createHeader() {
+		final Label header = new Label(getFieldEditorParent(), SWT.WRAP);
+		TextFormatter.setBoldFontStyle(header);
+		header.setText(Resources.get("lbl.preferences.webserver.header"));
+		return header;
+	}
 
 }
