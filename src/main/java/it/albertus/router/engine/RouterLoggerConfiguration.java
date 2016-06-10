a9f84d90e29edec4f205d63fe09c901997fdabfa
Albertus82 <albertus82@gmail.com>
2016-06-10 18:12:14 +0200

    Test.

--- src/main/java/it/albertus/router/engine/RouterLoggerConfiguration.java
+++ src/main/java/it/albertus/router/engine/RouterLoggerConfiguration.java
@@ -375,4 +375,30 @@ public class RouterLoggerConfiguration extends Configuration {
 		}
 	}
 
+//	public static void main(String... args) throws IOException {
+//		Properties p = new Properties();
+//		InputStream r = RouterLoggerConfiguration.class.getResourceAsStream("/routerlogger.cfg");
+//		p.load(r);
+//		r.close();
+//		System.out.println("Presenti in routerlogger.cfg e non presenti in Preference:");
+//		outer: for (String key : p.stringPropertyNames()) {
+//			for (Preference preference : Preference.values()) {
+//				if (preference.getConfigurationKey().equals(key)) {
+//					continue outer;
+//				}
+//			}
+//			System.out.println(key);
+//		}
+//		System.out.println();
+//		System.out.println("Presenti in Preference e non presenti in routerlogger.cfg:");
+//		outer: for (Preference preference : Preference.values()) {
+//			for (String key : p.stringPropertyNames()) {
+//				if (preference.getConfigurationKey().equals(key)) {
+//					continue outer;
+//				}
+//			}
+//			System.out.println(preference.getConfigurationKey());
+//		}
+//	}
+
 }
