1c3e09bdf769388cbb4747444ab7fc62f9832151
Albertus82 <albertus82@gmail.com>
2016-06-19 21:23:18 +0200

    Improved shell script.

--- src/main/scripts/routerlogger.sh
+++ src/main/scripts/routerlogger.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
+PRG="$0"
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+PRGDIR=`dirname "$PRG"`
 if [ "$1" = "" ]
   then if [ "$JAVA_HOME" != "" ]
-  then nice "$JAVA_HOME/bin/java" -Xms8m -Xmx64m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLogger
-  else nice java -Xms8m -Xmx64m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLogger
+  then nice "$JAVA_HOME/bin/java" -Xms8m -Xmx64m -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger
+  else nice java -Xms8m -Xmx64m -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx16m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLogger $1 $2
-  else java -Xms4m -Xmx16m -classpath "routerlogger.jar:lib/*" it.albertus.router.RouterLogger $1 $2
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx16m -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger $1 $2
+  else java -Xms4m -Xmx16m -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger $1 $2
   fi
 fi
