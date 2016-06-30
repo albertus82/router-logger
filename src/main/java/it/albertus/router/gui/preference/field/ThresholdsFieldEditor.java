2b71e265060f17f93d3f706dadc63ae390741a66
Albertus82 <albertus82@gmail.com>
2016-06-30 16:31:28 +0200

    Thresholds: identifiers can include any character.

--- src/main/java/it/albertus/router/gui/preference/field/ThresholdsFieldEditor.java
+++ src/main/java/it/albertus/router/gui/preference/field/ThresholdsFieldEditor.java
@@ -18,11 +18,10 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.VerifyEvent;
-import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
@@ -39,15 +38,19 @@ public class ThresholdsFieldEditor extends LocalizedListEditor {
 
 	@Override
 	protected void doLoad() {
-		if (getList() != null) {
+		final List list = getList();
+		if (list != null) {
 			final Set<String> thresholds = new TreeSet<String>();
 			for (final String preferenceName : getPreferenceStore().preferenceNames()) {
 				if (preferenceName.startsWith(Thresholds.CFG_PREFIX + '.')) {
-					thresholds.add(preferenceName.substring(preferenceName.indexOf('.') + 1) + '=' + getPreferenceStore().getString(preferenceName));
+					final String identifier = preferenceName.substring(preferenceName.indexOf('.') + 1);
+					final String expression = getPreferenceStore().getString(preferenceName);
+					thresholds.add(identifier + '=' + expression);
+					list.setData(identifier, expression);
 				}
 			}
 			for (final String threshold : thresholds) {
-				getList().add(threshold);
+				list.add(threshold);
 			}
 		}
 	}
@@ -89,8 +92,8 @@ public class ThresholdsFieldEditor extends LocalizedListEditor {
 	}
 
 	protected class ThresholdDialog extends TitleAreaDialog {
+
 		private static final int TEXT_LIMIT = 255;
-		private static final String REGEX_IDENTIFIER = "[0-9A-Za-z\\.]*";
 
 		private Text textIdentifier;
 		private Text textExpression;
@@ -131,7 +134,6 @@ public class ThresholdsFieldEditor extends LocalizedListEditor {
 			textIdentifier = new Text(container, SWT.BORDER);
 			GridDataFactory.swtDefaults().align(SWT.FILL, SWT.CENTER).grab(true, false).applyTo(textIdentifier);
 			textIdentifier.setTextLimit(TEXT_LIMIT);
-			textIdentifier.addVerifyListener(new IdentifierVerifyListener());
 
 			final Label labelExpression = new Label(container, SWT.NONE);
 			labelExpression.setText(Resources.get("lbl.preferences.thresholds.expressions.expression"));
@@ -167,7 +169,7 @@ public class ThresholdsFieldEditor extends LocalizedListEditor {
 
 		@Override
 		protected void okPressed() {
-			identifier = textIdentifier.getText();
+			identifier = textIdentifier.getText().trim();
 			expression = textExpression.getText();
 			super.okPressed();
 		}
@@ -183,7 +185,7 @@ public class ThresholdsFieldEditor extends LocalizedListEditor {
 		private class TextKeyListener extends KeyAdapter {
 			@Override
 			public void keyReleased(final KeyEvent ke) {
-				if (textIdentifier.getText().isEmpty() || textExpression.getText().trim().isEmpty()) {
+				if (textIdentifier.getText().isEmpty() || textExpression.getText().trim().isEmpty() || getList().getData(textIdentifier.getText().trim()) != null) {
 					if (okButton.isEnabled()) {
 						okButton.setEnabled(false);
 					}
@@ -192,15 +194,6 @@ public class ThresholdsFieldEditor extends LocalizedListEditor {
 					if (!okButton.isEnabled()) {
 						okButton.setEnabled(true);
 					}
-				}
-			}
-		}
-
-		private class IdentifierVerifyListener implements VerifyListener {
-			@Override
-			public void verifyText(final VerifyEvent ve) {
-				if (!ve.text.matches(REGEX_IDENTIFIER)) {
-					ve.doit = false;
 				}
 			}
 		}
