16478ece8b94644cefa32e1045309d7287ff5ec0
Albertus82 <albertus82@gmail.com>
2016-08-12 14:59:44 +0200

    Travis CI.

--- src/main/java/it/albertus/router/RouterLogger.java
+++ src/main/java/it/albertus/router/RouterLogger.java
@@ -3,7 +3,7 @@ package it.albertus.router;
 import it.albertus.router.console.RouterLoggerConsole;
 import it.albertus.router.gui.RouterLoggerGui;
 
-public class RouterLogger {_
+public class RouterLogger {
 
 	/** Unique entry point */
 	public static final void main(final String args[]) {
