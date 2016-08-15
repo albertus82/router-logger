4ae1b664f5f1ca365502be532e2659c46985ab32
Albertus82 <albertus82@gmail.com>
2016-08-15 22:51:19 +0200

    MQTT: Thresholds.

--- src/main/java/it/albertus/router/mqtt/ThresholdsPayload.java
+++ src/main/java/it/albertus/router/mqtt/ThresholdsPayload.java
@@ -1,53 +1,110 @@
 package it.albertus.router.mqtt;
 
 import it.albertus.router.engine.Threshold;
-import it.albertus.router.engine.Threshold.Type;
 import it.albertus.router.util.Jsonable;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import com.fasterxml.jackson.databind.util.ISO8601Utils;
 
 public class ThresholdsPayload implements Serializable, Jsonable {
 
-	private static final long serialVersionUID = -7741846352401497855L;
+	private static final long serialVersionUID = -566811797356824879L;
 
-	private class ThresholdItem {
+	private class ThresholdItem implements Serializable {
+
+		private static final long serialVersionUID = 1582125718825266945L;
+
+		private final Threshold threshold;
+
 		private final String name;
 		private final String key;
 		private final String type;
 		private final String value;
-		private final boolean excluded;
+		// private final boolean excluded;
 		private final String detected;
 
-		public ThresholdItem(final String name, final String key, final Type type, final String value, final boolean excluded, final String detected) {
-			this.name = name;
-			this.key = key;
-			this.type = type.name();
-			this.value = value;
-			this.excluded = excluded;
-			this.detected = detected;
+		public ThresholdItem(final Threshold threshold, final String value) {
+			this.threshold = threshold;
+			this.name = threshold.getName();
+			this.key = threshold.getKey();
+			this.type = threshold.getType().name();
+			this.value = threshold.getValue();
+			// this.excluded = threshold.isExcluded();
+			this.detected = value;
 		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + ((threshold == null) ? 0 : threshold.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof ThresholdItem)) {
+				return false;
+			}
+			ThresholdItem other = (ThresholdItem) obj;
+			if (!getOuterType().equals(other.getOuterType())) {
+				return false;
+			}
+			if (threshold == null) {
+				if (other.threshold != null) {
+					return false;
+				}
+			}
+			else if (!threshold.equals(other.threshold)) {
+				return false;
+			}
+			return true;
+		}
+
+		private ThresholdsPayload getOuterType() {
+			return ThresholdsPayload.this;
+		}
+
 	}
 
 	private final Date timestamp;
-	private final List<ThresholdItem> thresholds = new ArrayList<ThresholdItem>();
+	private final Set<ThresholdItem> thresholds;
 
 	public ThresholdsPayload(final Map<Threshold, String> thresholds, final Date timestamp) {
-		if (timestamp == null) {
-			this.timestamp = new Date();
-		}
-		else {
+		if (timestamp != null) {
 			this.timestamp = timestamp;
 		}
-		for (final Entry<Threshold, String> threshold : thresholds.entrySet()) {
-			this.thresholds.add(new ThresholdItem(threshold.getKey().getName(), threshold.getKey().getKey(), threshold.getKey().getType(), threshold.getKey().getValue(), threshold.getKey().isExcluded(), threshold.getValue()));
+		else {
+			this.timestamp = new Date();
 		}
+		if (thresholds != null) {
+			this.thresholds = new LinkedHashSet<ThresholdItem>(thresholds.size());
+			for (final Entry<Threshold, String> entry : thresholds.entrySet()) {
+				this.thresholds.add(new ThresholdItem(entry.getKey(), entry.getValue()));
+			}
+		}
+		else {
+			this.thresholds = Collections.emptySet();
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "ThresholdsPayload [timestamp=" + timestamp + ", thresholds=" + thresholds + "]";
 	}
 
 	@Override
@@ -57,7 +114,9 @@ public class ThresholdsPayload implements Serializable, Jsonable {
 			json.append(",\"thresholds\":[");
 			int index = 0;
 			for (final ThresholdItem threshold : thresholds) {
-				json.append("{\"name\":\"").append(threshold.name).append("\",\"key\":\"").append(threshold.key).append("\",\"type\":\"").append(threshold.type).append("\",\"value\":\"").append(threshold.value).append("\",\"excluded\":").append(threshold.excluded).append(",\"detected\":\"").append(threshold.detected).append("\"}");
+				json.append("{\"name\":\"").append(threshold.name).append("\",\"key\":\"").append(threshold.key).append("\",\"type\":\"").append(threshold.type).append("\",\"value\":\"").append(threshold.value).append("\"");
+				// json.append(",\"excluded\":").append(threshold.excluded);
+				json.append(",\"detected\":\"").append(threshold.detected).append("\"}");
 				if (++index < thresholds.size()) {
 					json.append(',');
 				}
