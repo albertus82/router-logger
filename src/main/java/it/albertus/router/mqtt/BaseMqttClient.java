b8564643dc6ecbc660bf9e2d0f7adcafb7f5be8f
Albertus82 <albertus82@gmail.com>
2016-08-20 23:43:59 +0200

    MQTT client: removed ambiguous method.

--- src/main/java/it/albertus/router/mqtt/BaseMqttClient.java
+++ src/main/java/it/albertus/router/mqtt/BaseMqttClient.java
@@ -121,10 +121,6 @@ public abstract class BaseMqttClient {
 		return payload;
 	}
 
-	public boolean isConnected() {
-		return client != null && client.isConnected();
-	}
-
 	public MqttClient getClient() {
 		return client;
 	}
