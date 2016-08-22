b9c1d744b22b69c302ab47ee826150afb04f9eaf
Albertus82 <albertus82@gmail.com>
2016-08-22 23:07:20 +0200

    DTOs.

---
+++ src/main/java/it/albertus/router/dto/ThresholdDto.java
@@ -0,0 +1,105 @@
+package it.albertus.router.dto;
+
+import it.albertus.router.engine.Threshold;
+
+import java.io.Serializable;
+
+public class ThresholdDto implements Serializable {
+
+	private static final long serialVersionUID = 3881838273700131909L;
+
+	private final String name;
+	private final String key;
+	private final String type;
+	private final String value;
+	private final boolean excluded;
+	private final String detected;
+
+	public ThresholdDto(final Threshold threshold, final String value) {
+		this.name = threshold.getName();
+		this.key = threshold.getKey();
+		this.type = threshold.getType().name();
+		this.value = threshold.getValue();
+		this.excluded = threshold.isExcluded();
+		this.detected = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public String getDetected() {
+		return detected;
+	}
+
+	public boolean isExcluded() {
+		return excluded;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof ThresholdDto)) {
+			return false;
+		}
+		ThresholdDto other = (ThresholdDto) obj;
+		if (key == null) {
+			if (other.key != null) {
+				return false;
+			}
+		}
+		else if (!key.equals(other.key)) {
+			return false;
+		}
+		if (type == null) {
+			if (other.type != null) {
+				return false;
+			}
+		}
+		else if (!type.equals(other.type)) {
+			return false;
+		}
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		}
+		else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ThresholdDto [name=" + name + ", key=" + key + ", type=" + type + ", value=" + value + ", excluded=" + excluded + ", detected=" + detected + "]";
+	}
+
+}
--- src/main/java/it/albertus/router/dto/ThresholdsDto.java
+++ src/main/java/it/albertus/router/dto/ThresholdsDto.java
@@ -14,17 +14,17 @@ import com.fasterxml.jackson.databind.util.ISO8601Utils;
 
 public class ThresholdsDto implements Serializable, Jsonable {
 
-	private static final long serialVersionUID = -566811797356824879L;
+	private static final long serialVersionUID = -7221966615926851395L;
 
 	private final Date timestamp;
-	private final Set<ThresholdItem> thresholds;
+	private final Set<ThresholdDto> thresholds;
 
 	public ThresholdsDto(final ThresholdsReached thresholdsReached) {
 		if (thresholdsReached != null) {
 			this.timestamp = thresholdsReached.getTimestamp();
-			this.thresholds = new LinkedHashSet<ThresholdItem>(thresholdsReached.getReached().size());
+			this.thresholds = new LinkedHashSet<ThresholdDto>(thresholdsReached.getReached().size());
 			for (final Entry<Threshold, String> entry : thresholdsReached.getReached().entrySet()) {
-				this.thresholds.add(new ThresholdItem(entry.getKey(), entry.getValue()));
+				this.thresholds.add(new ThresholdDto(entry.getKey(), entry.getValue()));
 			}
 		}
 		else {
@@ -49,9 +49,9 @@ public class ThresholdsDto implements Serializable, Jsonable {
 			json.append(",\"thresholds\":");
 			json.append("[");
 			int index = 0;
-			for (final ThresholdItem threshold : thresholds) {
+			for (final ThresholdDto threshold : thresholds) {
 				json.append("{\"name\":\"").append(threshold.getName()).append("\",\"key\":\"").append(threshold.getKey()).append("\",\"type\":\"").append(threshold.getType()).append("\",\"value\":\"").append(threshold.getValue()).append("\"");
-				// json.append(",\"excluded\":").append(threshold.excluded);
+				json.append(",\"excluded\":").append(threshold.isExcluded());
 				json.append(",\"detected\":\"").append(threshold.getDetected()).append("\"}");
 				if (++index < thresholds.size()) {
 					json.append(',');
@@ -60,86 +60,6 @@ public class ThresholdsDto implements Serializable, Jsonable {
 			json.append("]}");
 		}
 		return json.toString();
-	}
-
-}
-
-class ThresholdItem implements Serializable {
-
-	private static final long serialVersionUID = 1582125718825266945L;
-
-	private final Threshold threshold;
-	private final String key;
-	private final String type;
-	private final String value;
-	// private final boolean excluded;
-	private final String detected;
-
-	public ThresholdItem(final Threshold threshold, final String value) {
-		this.threshold = threshold;
-		this.name = threshold.getName();
-		this.key = threshold.getKey();
-		this.type = threshold.getType().name();
-		this.value = threshold.getValue();
-		// this.excluded = threshold.isExcluded();
-		this.detected = value;
-	}
-
-	private final String name;
-
-	public Threshold getThreshold() {
-		return threshold;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public String getDetected() {
-		return detected;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((threshold == null) ? 0 : threshold.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof ThresholdItem)) {
-			return false;
-		}
-		ThresholdItem other = (ThresholdItem) obj;
-		if (threshold == null) {
-			if (other.threshold != null) {
-				return false;
-			}
-		}
-		else if (!threshold.equals(other.threshold)) {
-			return false;
-		}
-		return true;
 	}
 
 }
