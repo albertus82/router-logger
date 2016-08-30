e5cc6876d93890b1b99cbd47d3c75331947e3949
Albertus82 <albertus82@gmail.com>
2016-08-30 23:26:40 +0200

    TODO improve enum.

--- src/main/java/it/albertus/router/gui/preference/page/RouterLoggerPage.java
+++ src/main/java/it/albertus/router/gui/preference/page/RouterLoggerPage.java
@@ -2,81 +2,97 @@ package it.albertus.router.gui.preference.page;
 
 import it.albertus.jface.preference.page.BasePreferencePage;
 import it.albertus.jface.preference.page.PageDefinition;
+import it.albertus.jface.preference.page.PageDefinitionData;
+import it.albertus.jface.preference.page.PageDefinitionData.PageDefinitionDataBuilder;
 import it.albertus.router.reader.AsusDslN12EReader;
 import it.albertus.router.reader.AsusDslN14UReader;
 import it.albertus.router.reader.DLinkDsl2750Reader;
 import it.albertus.router.reader.TpLink8970Reader;
 import it.albertus.router.resources.Resources;
+import it.albertus.util.Localized;
+
+import org.eclipse.jface.resource.ImageDescriptor;
 
 public enum RouterLoggerPage implements PageDefinition {
-	GENERAL(GeneralPreferencePage.class),
-	READER(ReaderPreferencePage.class),
-	APPEARANCE(RestartHeaderPreferencePage.class),
-	CONSOLE(null, APPEARANCE),
-	TPLINK_8970(TpLink8970Reader.DEVICE_MODEL_KEY, null, READER),
-	ASUS_N12E(AsusDslN12EReader.DEVICE_MODEL_KEY, null, READER),
-	ASUS_N14U(AsusDslN14UReader.DEVICE_MODEL_KEY, null, READER),
-	DLINK_2750(DLinkDsl2750Reader.DEVICE_MODEL_KEY, null, READER),
-	WRITER(WriterPreferencePage.class),
-	CSV(CsvPreferencePage.class, WRITER),
-	DATABASE(DatabasePreferencePage.class, WRITER),
+	GENERAL(new PageDefinitionDataBuilder().pageClass(GeneralPreferencePage.class).build()),
+	READER(new PageDefinitionDataBuilder().pageClass(ReaderPreferencePage.class).build()),
+	APPEARANCE(new PageDefinitionDataBuilder().pageClass(RestartHeaderPreferencePage.class).build()),
+	CONSOLE(new PageDefinitionDataBuilder().parent(APPEARANCE).build()),
+	TPLINK_8970(new PageDefinitionDataBuilder().label(new Localized() {
+		@Override
+		public String getString() {
+			return Resources.get(TpLink8970Reader.DEVICE_MODEL_KEY);
+		}
+	}).parent(READER).build()),
+	ASUS_N12E(new PageDefinitionDataBuilder().label(new Localized() {
+		@Override
+		public String getString() {
+			return Resources.get(AsusDslN12EReader.DEVICE_MODEL_KEY);
+		}
+	}).parent(READER).build()),
+	ASUS_N14U(new PageDefinitionDataBuilder().label(new Localized() {
+		@Override
+		public String getString() {
+			return Resources.get(AsusDslN14UReader.DEVICE_MODEL_KEY);
+		}
+	}).parent(READER).build()),
+	DLINK_2750(new PageDefinitionDataBuilder().label(new Localized() {
+		@Override
+		public String getString() {
+			return Resources.get(DLinkDsl2750Reader.DEVICE_MODEL_KEY);
+		}
+	}).parent(READER).build()),
+	WRITER(new PageDefinitionDataBuilder().pageClass(WriterPreferencePage.class).build()),
+	CSV(new PageDefinitionDataBuilder().pageClass(CsvPreferencePage.class).parent(WRITER).build()),
+	DATABASE(new PageDefinitionDataBuilder().pageClass(DatabasePreferencePage.class).parent(WRITER).build()),
 	THRESHOLDS(),
-	EMAIL(null),
-	EMAIL_ADVANCED(null, EMAIL),
-	EMAIL_CC_BCC(null, EMAIL),
-	SERVER(ServerPreferencePage.class),
-	SERVER_HANDLER(null, SERVER),
-	SERVER_HTTPS(ServerHttpsPreferencePage.class, SERVER),
-	MQTT(MqttPreferencePage.class),
-	MQTT_MESSAGES(RestartHeaderPreferencePage.class, MQTT),
-	MQTT_ADVANCED(AdvancedMqttPreferencePage.class, MQTT);
+	EMAIL(),
+	EMAIL_ADVANCED(new PageDefinitionDataBuilder().parent(EMAIL).build()),
+	EMAIL_CC_BCC(new PageDefinitionDataBuilder().parent(EMAIL).build()),
+	SERVER(new PageDefinitionDataBuilder().pageClass(ServerPreferencePage.class).build()),
+	SERVER_HANDLER(new PageDefinitionDataBuilder().parent(SERVER).build()),
+	SERVER_HTTPS(new PageDefinitionDataBuilder().pageClass(ServerHttpsPreferencePage.class).parent(SERVER).build()),
+	MQTT(new PageDefinitionDataBuilder().pageClass(MqttPreferencePage.class).build()),
+	MQTT_MESSAGES(new PageDefinitionDataBuilder().pageClass(RestartHeaderPreferencePage.class).parent(MQTT).build()),
+	MQTT_ADVANCED(new PageDefinitionDataBuilder().pageClass(AdvancedMqttPreferencePage.class).parent(MQTT).build());
 
 	private static final String LABEL_KEY_PREFIX = "lbl.preferences.";
 
 	private final String nodeId;
-	private final String labelKey;
+	private Localized label;
 	private final Class<? extends BasePreferencePage> pageClass;
+	private final ImageDescriptor image;
 	private final PageDefinition parent;
 
-	private RouterLoggerPage() {
-		this(null, null, null, null);
+	RouterLoggerPage() {
+		this(null);
 	}
 
-	private RouterLoggerPage(final Class<? extends BasePreferencePage> pageClass) {
-		this(null, null, pageClass, null);
+	RouterLoggerPage(final PageDefinitionData data) {
+		this(data != null ? data.getNodeId() : null, data != null ? data.getLabel() : null, data != null ? data.getPageClass() : null, data != null ? data.getImage() : null, data != null ? data.getParent() : null);
 	}
 
-	private RouterLoggerPage(final Class<? extends BasePreferencePage> pageClass, final PageDefinition parent) {
-		this(null, null, pageClass, parent);
-	}
-
-	private RouterLoggerPage(final String labelKey, final Class<? extends BasePreferencePage> pageClass) {
-		this(null, labelKey, pageClass, null);
-	}
-
-	private RouterLoggerPage(final String labelKey, final Class<? extends BasePreferencePage> pageClass, final PageDefinition parent) {
-		this(null, labelKey, pageClass, parent);
-	}
-
-	private RouterLoggerPage(final String nodeId, final String labelKey, final Class<? extends BasePreferencePage> pageClass) {
-		this(nodeId, labelKey, pageClass, null);
-	}
-
-	private RouterLoggerPage(final String nodeId, final String labelKey, final Class<? extends BasePreferencePage> pageClass, final PageDefinition parent) {
+	RouterLoggerPage(final String nodeId, final Localized label, final Class<? extends BasePreferencePage> pageClass, final ImageDescriptor image, final PageDefinition parent) {
 		if (nodeId != null && !nodeId.isEmpty()) {
 			this.nodeId = nodeId;
 		}
 		else {
 			this.nodeId = name().toLowerCase().replace('_', '.');
 		}
-		if (labelKey != null && !labelKey.isEmpty()) {
-			this.labelKey = labelKey;
+		if (label != null) {
+			this.label = label;
 		}
 		else {
-			this.labelKey = LABEL_KEY_PREFIX + this.nodeId;
+			this.label = new Localized() {
+				@Override
+				public String getString() {
+					return Resources.get(LABEL_KEY_PREFIX + RouterLoggerPage.this.nodeId);
+				}
+			};
 		}
 		this.pageClass = pageClass;
 		this.parent = parent;
+		this.image = image;
 	}
 
 	@Override
@@ -86,7 +102,7 @@ public enum RouterLoggerPage implements PageDefinition {
 
 	@Override
 	public String getLabel() {
-		return Resources.get(labelKey);
+		return label.getString();
 	}
 
 	@Override
@@ -97,6 +113,11 @@ public enum RouterLoggerPage implements PageDefinition {
 	@Override
 	public PageDefinition getParent() {
 		return parent;
+	}
+
+	@Override
+	public ImageDescriptor getImage() {
+		return image;
 	}
 
 }
