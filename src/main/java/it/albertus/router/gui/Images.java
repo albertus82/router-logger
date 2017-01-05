faca1360aa0a5c5072391bf754c55103147e5516
Albertus82 <albertus82@gmail.com>
2017-01-05 23:12:54 +0100

    minor fix

--- src/main/java/it/albertus/router/gui/Images.java
+++ src/main/java/it/albertus/router/gui/Images.java
@@ -1,6 +1,5 @@
 package it.albertus.router.gui;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -10,6 +9,8 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.swt.widgets.Display;
+
+import it.albertus.util.IOUtils;
 
 public class Images {
 
@@ -39,10 +40,7 @@ public class Images {
 	private static Image[] loadIcons(final String fileName) {
 		final InputStream is = Images.class.getResourceAsStream(fileName);
 		final ImageData[] images = new ImageLoader().load(is);
-		try {
-			is.close();
-		}
-		catch (final IOException ioe) {/* Ignore */}
+		IOUtils.closeQuietly(is);
 		final Image[] icons = new Image[images.length];
 		int i = 0;
 		for (final ImageData id : images) {
