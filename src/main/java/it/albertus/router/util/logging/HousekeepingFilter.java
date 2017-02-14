932477025c81967473ccbbeebf6b053ddc56e1be
Alberto <albertus82@gmail.com>
2017-02-14 09:56:03 +0100

    Revert "Removed logging from HousekeepingFilter."
    This reverts commit 37fe282a7d64fd332cfb5a30d9ce2a9dca3c5214.

--- src/main/java/it/albertus/router/util/logging/HousekeepingFilter.java
+++ src/main/java/it/albertus/router/util/logging/HousekeepingFilter.java
@@ -5,26 +5,25 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Filter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
+import it.albertus.router.resources.Messages;
 import it.albertus.router.util.FileSorter;
+import it.albertus.util.logging.LoggerFactory;
 import it.albertus.util.logging.TimeBasedRollingFileHandler;
 
 public class HousekeepingFilter implements Filter {
 
+	private static final Logger logger = LoggerFactory.getLogger(HousekeepingFilter.class);
+
 	private static final int MIN_HISTORY = 1;
 
 	private final int maxHistory;
-	private final String datePattern;
+	private DateFormat dateFormat;
 
 	private String currentFileNamePart;
-
-	private final ThreadLocal<DateFormat> dateFormat = new ThreadLocal<DateFormat>() {
-		@Override
-		protected DateFormat initialValue() {
-			return new SimpleDateFormat(datePattern);
-		}
-	};
 
 	public HousekeepingFilter(final int maxHistory) {
 		this(maxHistory, TimeBasedRollingFileHandler.Defaults.DATE_PATTERN);
@@ -32,17 +31,25 @@ public class HousekeepingFilter implements Filter {
 
 	public HousekeepingFilter(final int maxHistory, final String datePattern) {
 		if (maxHistory < MIN_HISTORY) {
+			logger.warning(Messages.get("err.log.housekeeping.maxHistory", MIN_HISTORY));
 			this.maxHistory = MIN_HISTORY;
 		}
 		else {
 			this.maxHistory = maxHistory;
 		}
-		this.datePattern = datePattern;
+		try {
+			this.dateFormat = new SimpleDateFormat(datePattern);
+		}
+		catch (final RuntimeException e) {
+			final String defaultDatePattern = TimeBasedRollingFileHandler.Defaults.DATE_PATTERN;
+			logger.log(Level.WARNING, Messages.get("err.log.housekeeping.datePattern", datePattern, defaultDatePattern), e);
+			this.dateFormat = new SimpleDateFormat(defaultDatePattern);
+		}
 	}
 
 	@Override
 	public synchronized boolean isLoggable(final LogRecord record) {
-		final String newFileNamePart = dateFormat.get().format(new Date());
+		final String newFileNamePart = dateFormat.format(new Date());
 		if (!newFileNamePart.equals(currentFileNamePart)) {
 			int keep = this.maxHistory;
 			if (currentFileNamePart == null) {
@@ -59,14 +66,12 @@ public class HousekeepingFilter implements Filter {
 		if (files != null && files.length > keep) {
 			FileSorter.sortByLastModified(files);
 			for (int i = 0; i < files.length - keep; i++) {
-				LogManager.deleteFile(files[i]);
+				final boolean deleted = LogManager.deleteFile(files[i]);
+				if (deleted) {
+					logger.info(Messages.get("msg.log.file.deleted", files[i]));
+				}
 			}
 		}
-	}
-
-	@Override
-	public String toString() {
-		return "HousekeepingFilter [maxHistory=" + maxHistory + ", datePattern=" + datePattern + "]";
 	}
 
 }
