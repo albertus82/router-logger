e62a1fa69cccb6b5699b64a6fcb02f2cfc583b77
Alberto <albertus82@gmail.com>
2017-02-14 22:18:04 +0100

    Refactoring.

--- src/main/java/it/albertus/router/RouterLogger.java
+++ src/main/java/it/albertus/router/RouterLogger.java
@@ -31,8 +31,7 @@ public class RouterLogger {
 	private static InitializationException initializationException = null;
 
 	static {
-		final Logger rootLogger = LoggingSupport.getRootLogger();
-		for (final Handler handler : rootLogger.getHandlers()) {
+		for (final Handler handler : LoggingSupport.getRootHandlers()) {
 			if (handler instanceof ConsoleHandler) {
 				handler.setFormatter(new ConsoleFormatter("%1$td/%1$tm/%1$tY %1$tH:%1$tM:%1$tS.%tL %4$s: %5$s%6$s%n"));
 			}
