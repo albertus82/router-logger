05beee52a33d1e13799f4c3da555a7f0a7014ee5
Alberto <albertus82@gmail.com>
2017-02-15 16:08:00 +0100

    Revert "Java 9 compatibility (to be tested)."
    This reverts commit c0781568989056f8db5edce8f65ae2f7cc0b70be.

--- src/main/scripts/routerlogger.sh
+++ src/main/scripts/routerlogger.sh
@@ -12,12 +12,12 @@ done
 PRGDIR=`dirname "$PRG"`
 if [ "$1" = "" ]
   then if [ "$JAVA_HOME" != "" ]
-  then nice "$JAVA_HOME/bin/java" -Xms8m -Xmx64m --add-modules=java.activation -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger
-  else nice java -Xms8m -Xmx64m --add-modules=java.activation -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger
+  then nice "$JAVA_HOME/bin/java" -Xms8m -Xmx64m -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger
+  else nice java -Xms8m -Xmx64m -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx16m --add-modules=java.activation -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger $1 $2
-  else java -Xms4m -Xmx16m --add-modules=java.activation -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger $1 $2
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx16m -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger $1 $2
+  else java -Xms4m -Xmx16m -classpath "$PRGDIR/routerlogger.jar:$PRGDIR/lib/*" it.albertus.router.RouterLogger $1 $2
   fi
 fi
