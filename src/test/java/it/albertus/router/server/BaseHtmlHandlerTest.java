97d0fd1695a7a1e4448e499e777716fee032ea91
Alberto <albertus82@gmail.com>
2017-03-12 13:26:08 +0100

    Test

--- src/test/java/it/albertus/router/server/BaseHtmlHandlerTest.java
+++ src/test/java/it/albertus/router/server/BaseHtmlHandlerTest.java
@@ -7,14 +7,14 @@ import it.albertus.router.server.html.BaseHtmlHandler;
 
 public class BaseHtmlHandlerTest {
 
-	private static final String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, mi porttitor lacinia cursus, elit lectus vehicula ante, vitae molestie lacus lectus nec nulla. Cras aliquet mi vitae dui porttitor dignissim. Mauris varius, sem eget tempus finibus, magna odio ultricies ipsum, a luctus turpis lorem nec elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed quis neque non turpis scelerisque sagittis ut eget purus. Etiam bibendum, lectus eget interdum rutrum, est diam tristique neque, vitae consectetur elit lorem sit amet augue. Vivamus in sodales metus. Vivamus arcu quam, rutrum vitae ullamcorper laoreet, condimentum sit amet metus. Duis quis ligula eleifend, maximus est in, tristique justo. Morbi felis purus, luctus et tincidunt vel, ultrices viverra dui.";
-	private static final String specials = "'qwertyuiop' & <<asdfghjkl>> && \"zxcvbnm,._-\"";
+	private static final String petrarca = "e 'l viso di pietosi color farsi,\r\nnon so se vero o falso, mi parea:\r\ni' che l'esca amorosa al petto avea,\r\nqual meraviglia se di subito arsi?";
+	private static final String specials = "'qwertyuiop' & <<asdfghjkl>> && \"\"\"zxcvbnm\"\"\"";
 
 	@Test
 	public void escapeHtmlTest() {
-		String escaped = BaseHtmlHandler.escapeHtml(lorem);
+		String escaped = BaseHtmlHandler.escapeHtml(petrarca);
 		System.out.println(escaped);
-		Assert.assertEquals(lorem, BaseHtmlHandler.unescapeHtml(escaped));
+		Assert.assertEquals(petrarca, BaseHtmlHandler.unescapeHtml(escaped));
 
 		escaped = BaseHtmlHandler.escapeHtml(specials);
 		System.out.println(escaped);
