272f5174e435a39a877d1bb9eec918b145170a83
Alberto <albertus82@gmail.com>
2017-08-07 11:18:56 +0200

    SystemPropertiesDialog.

--- src/main/java/it/albertus/router/gui/MenuBar.java
+++ src/main/java/it/albertus/router/gui/MenuBar.java
@@ -11,6 +11,7 @@ import org.eclipse.swt.widgets.MenuItem;
 import it.albertus.jface.SwtUtils;
 import it.albertus.jface.cocoa.CocoaEnhancerException;
 import it.albertus.jface.cocoa.CocoaUIEnhancer;
+import it.albertus.jface.listener.SystemPropertiesListener;
 import it.albertus.router.gui.listener.AboutListener;
 import it.albertus.router.gui.listener.ClearConsoleSelectionListener;
 import it.albertus.router.gui.listener.ClearDataTableSelectionListener;
@@ -22,6 +23,7 @@ import it.albertus.router.gui.listener.DeleteDataTableSelectionListener;
 import it.albertus.router.gui.listener.DisconnectSelectionListener;
 import it.albertus.router.gui.listener.EditClearSubMenuListener;
 import it.albertus.router.gui.listener.EditMenuListener;
+import it.albertus.router.gui.listener.HelpMenuListener;
 import it.albertus.router.gui.listener.PreferencesListener;
 import it.albertus.router.gui.listener.RestartSelectionListener;
 import it.albertus.router.gui.listener.SelectAllMenuBarSelectionListener;
@@ -36,6 +38,27 @@ import it.albertus.util.logging.LoggerFactory;
  * combinazioni di tasti saranno ignorate.
  */
 public class MenuBar {
+
+	private static final String LBL_MENU_HEADER_FILE = "lbl.menu.header.file";
+	private static final String LBL_MENU_ITEM_RESTART = "lbl.menu.item.restart";
+	private static final String LBL_MENU_ITEM_EXIT = "lbl.menu.item.exit";
+	private static final String LBL_MENU_HEADER_EDIT = "lbl.menu.header.edit";
+	private static final String LBL_MENU_ITEM_COPY = "lbl.menu.item.copy";
+	private static final String LBL_MENU_ITEM_DELETE = "lbl.menu.item.delete";
+	private static final String LBL_MENU_ITEM_DELETE_KEY = "lbl.menu.item.delete.key";
+	private static final String LBL_MENU_ITEM_SELECT_ALL = "lbl.menu.item.select.all";
+	private static final String LBL_MENU_ITEM_CLEAR = "lbl.menu.item.clear";
+	private static final String LBL_MENU_ITEM_CLEAR_TABLE = "lbl.menu.item.clear.table";
+	private static final String LBL_MENU_ITEM_CLEAR_CONSOLE = "lbl.menu.item.clear.console";
+	private static final String LBL_MENU_HEADER_CONNECTION = "lbl.menu.header.connection";
+	private static final String LBL_MENU_ITEM_CONNECT = "lbl.menu.item.connect";
+	private static final String LBL_MENU_ITEM_DISCONNECT = "lbl.menu.item.disconnect";
+	private static final String LBL_MENU_HEADER_TOOLS = "lbl.menu.header.tools";
+	private static final String LBL_MENU_ITEM_PREFERENCES = "lbl.menu.item.preferences";
+	private static final String LBL_MENU_HEADER_HELP = "lbl.menu.header.help";
+	private static final String LBL_MENU_HEADER_HELP_COCOA = "lbl.menu.header.help.cocoa";
+	private static final String LBL_MENU_ITEM_SYSTEM_PROPERTIES = "lbl.menu.item.system.properties";
+	private static final String LBL_MENU_ITEM_ABOUT = "lbl.menu.item.about";
 
 	private static final Logger logger = LoggerFactory.getLogger(MenuBar.class);
 
@@ -66,8 +89,9 @@ public class MenuBar {
 	private MenuItem toolsMenuHeader;
 	private MenuItem toolsPreferencesMenuItem;
 
-	private Menu helpMenu;
-	private MenuItem helpMenuHeader;
+	private final Menu helpMenu;
+	private final MenuItem helpMenuHeader;
+	private final MenuItem helpSystemPropertiesItem;
 	private MenuItem helpAboutItem;
 
 	protected MenuBar(final RouterLoggerGui gui) {
@@ -88,54 +112,54 @@ public class MenuBar {
 
 		bar = new Menu(gui.getShell(), SWT.BAR); // Barra
 
-		/* File */
+		// File
 		fileMenu = new Menu(gui.getShell(), SWT.DROP_DOWN);
 		fileMenuHeader = new MenuItem(bar, SWT.CASCADE);
-		fileMenuHeader.setText(Messages.get("lbl.menu.header.file"));
+		fileMenuHeader.setText(Messages.get(LBL_MENU_HEADER_FILE));
 		fileMenuHeader.setMenu(fileMenu);
 
 		fileRestartItem = new MenuItem(fileMenu, SWT.PUSH);
-		fileRestartItem.setText(Messages.get("lbl.menu.item.restart"));
+		fileRestartItem.setText(Messages.get(LBL_MENU_ITEM_RESTART));
 		fileRestartItem.addSelectionListener(new RestartSelectionListener(gui));
 
 		if (!cocoaMenuCreated) {
 			new MenuItem(fileMenu, SWT.SEPARATOR);
 
 			fileExitItem = new MenuItem(fileMenu, SWT.PUSH);
-			fileExitItem.setText(Messages.get("lbl.menu.item.exit"));
+			fileExitItem.setText(Messages.get(LBL_MENU_ITEM_EXIT));
 			fileExitItem.addSelectionListener(new CloseListener(gui));
 		}
 
-		/* Edit */
+		// Edit
 		editMenu = new Menu(gui.getShell(), SWT.DROP_DOWN);
 		editMenuHeader = new MenuItem(bar, SWT.CASCADE);
-		editMenuHeader.setText(Messages.get("lbl.menu.header.edit"));
+		editMenuHeader.setText(Messages.get(LBL_MENU_HEADER_EDIT));
 		editMenuHeader.setMenu(editMenu);
 		final EditMenuListener editMenuListener = new EditMenuListener(gui);
 		editMenu.addMenuListener(editMenuListener);
 		editMenuHeader.addArmListener(editMenuListener);
 
 		editCopyMenuItem = new MenuItem(editMenu, SWT.PUSH);
-		editCopyMenuItem.setText(Messages.get("lbl.menu.item.copy") + SwtUtils.getMod1ShortcutLabel(SwtUtils.KEY_COPY));
+		editCopyMenuItem.setText(Messages.get(LBL_MENU_ITEM_COPY) + SwtUtils.getMod1ShortcutLabel(SwtUtils.KEY_COPY));
 		editCopyMenuItem.addSelectionListener(new CopyMenuBarSelectionListener(gui));
 		editCopyMenuItem.setAccelerator(SWT.MOD1 | SwtUtils.KEY_COPY); // Vero!
 
 		editDeleteMenuItem = new MenuItem(editMenu, SWT.PUSH);
-		editDeleteMenuItem.setText(Messages.get("lbl.menu.item.delete") + SwtUtils.getShortcutLabel(Messages.get("lbl.menu.item.delete.key")));
+		editDeleteMenuItem.setText(Messages.get(LBL_MENU_ITEM_DELETE) + SwtUtils.getShortcutLabel(Messages.get(LBL_MENU_ITEM_DELETE_KEY)));
 		editDeleteMenuItem.addSelectionListener(new DeleteDataTableSelectionListener(gui));
 		editDeleteMenuItem.setAccelerator(SwtUtils.KEY_DELETE); // Vero!
 
 		new MenuItem(editMenu, SWT.SEPARATOR);
 
 		editSelectAllMenuItem = new MenuItem(editMenu, SWT.PUSH);
-		editSelectAllMenuItem.setText(Messages.get("lbl.menu.item.select.all") + SwtUtils.getMod1ShortcutLabel(SwtUtils.KEY_SELECT_ALL));
+		editSelectAllMenuItem.setText(Messages.get(LBL_MENU_ITEM_SELECT_ALL) + SwtUtils.getMod1ShortcutLabel(SwtUtils.KEY_SELECT_ALL));
 		editSelectAllMenuItem.addSelectionListener(new SelectAllMenuBarSelectionListener(gui));
 		editSelectAllMenuItem.setAccelerator(SWT.MOD1 | SwtUtils.KEY_SELECT_ALL); // Vero!
 
 		new MenuItem(editMenu, SWT.SEPARATOR);
 
 		editClearSubMenuItem = new MenuItem(editMenu, SWT.CASCADE);
-		editClearSubMenuItem.setText(Messages.get("lbl.menu.item.clear"));
+		editClearSubMenuItem.setText(Messages.get(LBL_MENU_ITEM_CLEAR));
 
 		editClearSubMenu = new Menu(gui.getShell(), SWT.DROP_DOWN);
 		editClearSubMenuItem.setMenu(editClearSubMenu);
@@ -144,86 +168,95 @@ public class MenuBar {
 		editClearSubMenuItem.addArmListener(editClearSubMenuListener);
 
 		editClearDataTableMenuItem = new MenuItem(editClearSubMenu, SWT.PUSH);
-		editClearDataTableMenuItem.setText(Messages.get("lbl.menu.item.clear.table"));
+		editClearDataTableMenuItem.setText(Messages.get(LBL_MENU_ITEM_CLEAR_TABLE));
 		editClearDataTableMenuItem.addSelectionListener(new ClearDataTableSelectionListener(gui));
 
 		editClearConsoleMenuItem = new MenuItem(editClearSubMenu, SWT.PUSH);
-		editClearConsoleMenuItem.setText(Messages.get("lbl.menu.item.clear.console"));
+		editClearConsoleMenuItem.setText(Messages.get(LBL_MENU_ITEM_CLEAR_CONSOLE));
 		editClearConsoleMenuItem.addSelectionListener(new ClearConsoleSelectionListener(gui));
 
-		/* Connection */
+		// Connection
 		connectionMenu = new Menu(gui.getShell(), SWT.DROP_DOWN);
 		connectionMenuHeader = new MenuItem(bar, SWT.CASCADE);
-		connectionMenuHeader.setText(Messages.get("lbl.menu.header.connection"));
+		connectionMenuHeader.setText(Messages.get(LBL_MENU_HEADER_CONNECTION));
 		connectionMenuHeader.setMenu(connectionMenu);
 		final ConnectionMenuListener connectionMenuListener = new ConnectionMenuListener(gui);
 		connectionMenu.addMenuListener(connectionMenuListener);
 		connectionMenuHeader.addArmListener(connectionMenuListener);
 
 		connectionConnectItem = new MenuItem(connectionMenu, SWT.PUSH);
-		connectionConnectItem.setText(Messages.get("lbl.menu.item.connect"));
+		connectionConnectItem.setText(Messages.get(LBL_MENU_ITEM_CONNECT));
 		connectionConnectItem.addSelectionListener(new ConnectSelectionListener(gui));
 
 		new MenuItem(connectionMenu, SWT.SEPARATOR);
 
 		connectionDisconnectItem = new MenuItem(connectionMenu, SWT.PUSH);
-		connectionDisconnectItem.setText(Messages.get("lbl.menu.item.disconnect"));
+		connectionDisconnectItem.setText(Messages.get(LBL_MENU_ITEM_DISCONNECT));
 		connectionDisconnectItem.addSelectionListener(new DisconnectSelectionListener(gui));
 
-		/* Tools */
+		// Tools
 		if (!cocoaMenuCreated) {
 			toolsMenu = new Menu(gui.getShell(), SWT.DROP_DOWN);
 			toolsMenuHeader = new MenuItem(bar, SWT.CASCADE);
-			toolsMenuHeader.setText(Messages.get("lbl.menu.header.tools"));
+			toolsMenuHeader.setText(Messages.get(LBL_MENU_HEADER_TOOLS));
 			toolsMenuHeader.setMenu(toolsMenu);
 
 			toolsPreferencesMenuItem = new MenuItem(toolsMenu, SWT.PUSH);
-			toolsPreferencesMenuItem.setText(Messages.get("lbl.menu.item.preferences"));
+			toolsPreferencesMenuItem.setText(Messages.get(LBL_MENU_ITEM_PREFERENCES));
 			toolsPreferencesMenuItem.addSelectionListener(new PreferencesListener(gui));
 		}
 
-		/* Help */
+		// Help
+		helpMenu = new Menu(gui.getShell(), SWT.DROP_DOWN);
+		helpMenuHeader = new MenuItem(bar, SWT.CASCADE);
+		helpMenuHeader.setText(Messages.get(Util.isCocoa() ? LBL_MENU_HEADER_HELP_COCOA : LBL_MENU_HEADER_HELP));
+		helpMenuHeader.setMenu(helpMenu);
+
+		helpSystemPropertiesItem = new MenuItem(helpMenu, SWT.PUSH);
+		helpSystemPropertiesItem.setText(Messages.get(LBL_MENU_ITEM_SYSTEM_PROPERTIES));
+		helpSystemPropertiesItem.addSelectionListener(new SystemPropertiesListener(gui));
+
 		if (!cocoaMenuCreated) {
-			helpMenu = new Menu(gui.getShell(), SWT.DROP_DOWN);
-			helpMenuHeader = new MenuItem(bar, SWT.CASCADE);
-			helpMenuHeader.setText(Messages.get("lbl.menu.header.help"));
-			helpMenuHeader.setMenu(helpMenu);
+			new MenuItem(helpMenu, SWT.SEPARATOR);
 
 			helpAboutItem = new MenuItem(helpMenu, SWT.PUSH);
-			helpAboutItem.setText(Messages.get("lbl.menu.item.about"));
+			helpAboutItem.setText(Messages.get(LBL_MENU_ITEM_ABOUT));
 			helpAboutItem.addSelectionListener(new AboutListener(gui));
 		}
+
+		final HelpMenuListener helpMenuListener = new HelpMenuListener(helpSystemPropertiesItem);
+		helpMenu.addMenuListener(helpMenuListener);
+		helpMenuHeader.addArmListener(helpMenuListener);
 
 		gui.getShell().setMenuBar(bar);
 	}
 
 	public void updateTexts() {
-		fileMenuHeader.setText(Messages.get("lbl.menu.header.file"));
-		fileRestartItem.setText(Messages.get("lbl.menu.item.restart"));
+		fileMenuHeader.setText(Messages.get(LBL_MENU_HEADER_FILE));
+		fileRestartItem.setText(Messages.get(LBL_MENU_ITEM_RESTART));
 		if (fileExitItem != null && !fileExitItem.isDisposed()) {
-			fileExitItem.setText(Messages.get("lbl.menu.item.exit"));
+			fileExitItem.setText(Messages.get(LBL_MENU_ITEM_EXIT));
 		}
-		editMenuHeader.setText(Messages.get("lbl.menu.header.edit"));
-		editCopyMenuItem.setText(Messages.get("lbl.menu.item.copy") + SwtUtils.getMod1ShortcutLabel(SwtUtils.KEY_COPY));
-		editDeleteMenuItem.setText(Messages.get("lbl.menu.item.delete") + SwtUtils.getShortcutLabel(Messages.get("lbl.menu.item.delete.key")));
-		editSelectAllMenuItem.setText(Messages.get("lbl.menu.item.select.all") + SwtUtils.getMod1ShortcutLabel(SwtUtils.KEY_SELECT_ALL));
-		editClearSubMenuItem.setText(Messages.get("lbl.menu.item.clear"));
-		editClearDataTableMenuItem.setText(Messages.get("lbl.menu.item.clear.table"));
-		editClearConsoleMenuItem.setText(Messages.get("lbl.menu.item.clear.console"));
-		connectionMenuHeader.setText(Messages.get("lbl.menu.header.connection"));
-		connectionConnectItem.setText(Messages.get("lbl.menu.item.connect"));
-		connectionDisconnectItem.setText(Messages.get("lbl.menu.item.disconnect"));
+		editMenuHeader.setText(Messages.get(LBL_MENU_HEADER_EDIT));
+		editCopyMenuItem.setText(Messages.get(LBL_MENU_ITEM_COPY) + SwtUtils.getMod1ShortcutLabel(SwtUtils.KEY_COPY));
+		editDeleteMenuItem.setText(Messages.get(LBL_MENU_ITEM_DELETE) + SwtUtils.getShortcutLabel(Messages.get(LBL_MENU_ITEM_DELETE_KEY)));
+		editSelectAllMenuItem.setText(Messages.get(LBL_MENU_ITEM_SELECT_ALL) + SwtUtils.getMod1ShortcutLabel(SwtUtils.KEY_SELECT_ALL));
+		editClearSubMenuItem.setText(Messages.get(LBL_MENU_ITEM_CLEAR));
+		editClearDataTableMenuItem.setText(Messages.get(LBL_MENU_ITEM_CLEAR_TABLE));
+		editClearConsoleMenuItem.setText(Messages.get(LBL_MENU_ITEM_CLEAR_CONSOLE));
+		connectionMenuHeader.setText(Messages.get(LBL_MENU_HEADER_CONNECTION));
+		connectionConnectItem.setText(Messages.get(LBL_MENU_ITEM_CONNECT));
+		connectionDisconnectItem.setText(Messages.get(LBL_MENU_ITEM_DISCONNECT));
 		if (toolsMenuHeader != null && !toolsMenuHeader.isDisposed()) {
-			toolsMenuHeader.setText(Messages.get("lbl.menu.header.tools"));
+			toolsMenuHeader.setText(Messages.get(LBL_MENU_HEADER_TOOLS));
 		}
 		if (toolsPreferencesMenuItem != null && !toolsPreferencesMenuItem.isDisposed()) {
-			toolsPreferencesMenuItem.setText(Messages.get("lbl.menu.item.preferences"));
+			toolsPreferencesMenuItem.setText(Messages.get(LBL_MENU_ITEM_PREFERENCES));
 		}
-		if (helpMenuHeader != null && !helpMenuHeader.isDisposed()) {
-			helpMenuHeader.setText(Messages.get("lbl.menu.header.help"));
-		}
+		helpMenuHeader.setText(Messages.get(Util.isCocoa() ? LBL_MENU_HEADER_HELP_COCOA : LBL_MENU_HEADER_HELP));
+		helpSystemPropertiesItem.setText(Messages.get(LBL_MENU_ITEM_SYSTEM_PROPERTIES));
 		if (helpAboutItem != null && !helpAboutItem.isDisposed()) {
-			helpAboutItem.setText(Messages.get("lbl.menu.item.about"));
+			helpAboutItem.setText(Messages.get(LBL_MENU_ITEM_ABOUT));
 		}
 	}
 
---
+++ src/main/java/it/albertus/router/gui/listener/HelpMenuListener.java
@@ -0,0 +1,50 @@
+package it.albertus.router.gui.listener;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.eclipse.swt.events.ArmEvent;
+import org.eclipse.swt.events.ArmListener;
+import org.eclipse.swt.events.MenuEvent;
+import org.eclipse.swt.events.MenuListener;
+import org.eclipse.swt.widgets.MenuItem;
+
+import it.albertus.util.logging.LoggerFactory;
+
+public class HelpMenuListener implements ArmListener, MenuListener {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelpMenuListener.class);
+
+	private final MenuItem item;
+
+	public HelpMenuListener(final MenuItem item) {
+		this.item = item;
+	}
+
+	@Override
+	public void widgetArmed(final ArmEvent e) {
+		execute();
+	}
+
+	@Override
+	public void menuShown(final MenuEvent e) {
+		execute();
+	}
+
+	@Override
+	public void menuHidden(final MenuEvent e) {/* Ignore */}
+
+	private void execute() {
+		final SecurityManager securityManager = System.getSecurityManager();
+		if (securityManager != null) {
+			try {
+				securityManager.checkPropertiesAccess();
+			}
+			catch (final SecurityException e) {
+				logger.log(Level.FINE, e.toString(), e);
+				item.setEnabled(false);
+			}
+		}
+	}
+
+}
