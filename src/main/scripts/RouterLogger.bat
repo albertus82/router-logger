90a0e9c866f2a46de4e2fe485ef8c8f6c20c0ff5
Alberto <albertus82@gmail.com>
2017-10-15 17:36:51 +0200

    Improved Java 9 compatibility: added activation-1.1.1.jar

--- src/main/scripts/RouterLogger.bat
+++ src/main/scripts/RouterLogger.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 IF "%1" == "" GOTO GUI
 :CON
-IF "%JAVA_HOME%" == "" java.exe -Xms4m -Xmx16m -XX:+IgnoreUnrecognizedVMOptions --add-modules=java.activation -classpath "%~dp0RouterLogger.jar;%~dp0dropins\*;%~dp0lib\*" it.albertus.routerlogger.RouterLogger %1 %2
-IF NOT "%JAVA_HOME%" == "" "%JAVA_HOME%\bin\java.exe" -Xms4m -Xmx16m -XX:+IgnoreUnrecognizedVMOptions --add-modules=java.activation -classpath "%~dp0RouterLogger.jar;%~dp0dropins\*;%~dp0lib\*" it.albertus.routerlogger.RouterLogger %1 %2
+IF "%JAVA_HOME%" == "" java.exe -Xms4m -Xmx16m -classpath "%~dp0RouterLogger.jar;%~dp0dropins\*;%~dp0lib\*" it.albertus.routerlogger.RouterLogger %1 %2
+IF NOT "%JAVA_HOME%" == "" "%JAVA_HOME%\bin\java.exe" -Xms4m -Xmx16m -classpath "%~dp0RouterLogger.jar;%~dp0dropins\*;%~dp0lib\*" it.albertus.routerlogger.RouterLogger %1 %2
 GOTO END
 :GUI
-IF "%JAVA_HOME%" == "" START /BELOWNORMAL "" javaw.exe -Xms8m -Xmx64m -XX:+IgnoreUnrecognizedVMOptions --add-modules=java.activation -classpath "%~dp0RouterLogger.jar;%~dp0dropins\*;%~dp0lib\*" it.albertus.routerlogger.RouterLogger
-IF NOT "%JAVA_HOME%" == "" START /BELOWNORMAL "" "%JAVA_HOME%\bin\javaw.exe" -Xms8m -Xmx64m -XX:+IgnoreUnrecognizedVMOptions --add-modules=java.activation -classpath "%~dp0RouterLogger.jar;%~dp0dropins\*;%~dp0lib\*" it.albertus.routerlogger.RouterLogger
+IF "%JAVA_HOME%" == "" START /BELOWNORMAL "" javaw.exe -Xms8m -Xmx64m -classpath "%~dp0RouterLogger.jar;%~dp0dropins\*;%~dp0lib\*" it.albertus.routerlogger.RouterLogger
+IF NOT "%JAVA_HOME%" == "" START /BELOWNORMAL "" "%JAVA_HOME%\bin\javaw.exe" -Xms8m -Xmx64m -classpath "%~dp0RouterLogger.jar;%~dp0dropins\*;%~dp0lib\*" it.albertus.routerlogger.RouterLogger
 GOTO END
 :END
