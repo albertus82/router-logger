21f3711fca703b7ea1e08f7c05cf604c9a698918
Alberto <albertus82@gmail.com>
2017-11-08 09:51:39 +0100

    Images

--- src/main/java/it/albertus/routerlogger/gui/Images.java
+++ src/main/java/it/albertus/routerlogger/gui/Images.java
@@ -1,6 +1,8 @@
 package it.albertus.routerlogger.gui;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.DecorationOverlayIcon;
@@ -14,18 +16,18 @@ import it.albertus.util.IOUtils;
 
 public class Images {
 
-	// Icona principale dell'applicazione (in vari formati)
-	private static final Image[] MAIN_ICONS = loadIcons("main.ico");
+	// Main application icon (in various formats)
+	private static final List<Image> mainIcons = load("main.ico");
 
 	// Icone base per l'area di notifica (16x16)
-	public static final Image TRAY_ICON_ACTIVE = Images.MAIN_ICONS[2];
-	public static final Image TRAY_ICON_INACTIVE = loadIcons("inactive.ico")[0];
+	public static final Image TRAY_ICON_ACTIVE = mainIcons.get(2);
+	public static final Image TRAY_ICON_INACTIVE = load("inactive.ico").get(0);
 
 	// Simboli in sovraimpressione per l'area di notifica (16x16, non utilizzabili da soli)
-	private static final Image TRAY_ICON_OVERLAY_CLOCK = loadIcons("clock.ico")[0];
-	private static final Image TRAY_ICON_OVERLAY_ERROR = loadIcons("error.ico")[0];
-	private static final Image TRAY_ICON_OVERLAY_LOCK = loadIcons("lock.ico")[0];
-	private static final Image TRAY_ICON_OVERLAY_WARNING = loadIcons("warning.ico")[0];
+	private static final Image TRAY_ICON_OVERLAY_CLOCK = load("clock.ico").get(0);
+	private static final Image TRAY_ICON_OVERLAY_ERROR = load("error.ico").get(0);
+	private static final Image TRAY_ICON_OVERLAY_LOCK = load("lock.ico").get(0);
+	private static final Image TRAY_ICON_OVERLAY_WARNING = load("warning.ico").get(0);
 
 	// Icone composte per l'area di notifica (16x16)
 	public static final Image TRAY_ICON_ACTIVE_WARNING = new DecorationOverlayIcon(TRAY_ICON_ACTIVE, ImageDescriptor.createFromImage(Images.TRAY_ICON_OVERLAY_WARNING), IDecoration.BOTTOM_RIGHT).createImage();
@@ -37,20 +39,23 @@ public class Images {
 		throw new IllegalAccessError();
 	}
 
-	private static Image[] loadIcons(final String fileName) {
-		final InputStream is = Images.class.getResourceAsStream(fileName);
-		final ImageData[] images = new ImageLoader().load(is);
-		IOUtils.closeQuietly(is);
-		final Image[] icons = new Image[images.length];
-		int i = 0;
-		for (final ImageData id : images) {
-			icons[i++] = new Image(Display.getCurrent(), id);
+	private static List<Image> load(final String fileName) {
+		InputStream is = null;
+		try {
+			is = Images.class.getResourceAsStream(fileName);
+			final List<Image> images = new ArrayList<Image>();
+			for (final ImageData id : new ImageLoader().load(is)) {
+				images.add(new Image(Display.getCurrent(), id));
+			}
+			return images;
 		}
-		return icons;
+		finally {
+			IOUtils.closeQuietly(is);
+		}
 	}
 
 	public static Image[] getMainIcons() {
-		return MAIN_ICONS;
+		return mainIcons.toArray(new Image[mainIcons.size()]);
 	}
 
 }
