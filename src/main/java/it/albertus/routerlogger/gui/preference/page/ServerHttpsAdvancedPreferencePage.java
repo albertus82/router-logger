52dcb1b572b295b8f8a5eb7f6ba4ab43f5feecca
Alberto <albertus82@gmail.com>
2017-11-11 22:38:50 +0100

    Improved RedirectStatusComboOptions

--- src/main/java/it/albertus/routerlogger/gui/preference/page/ServerHttpsAdvancedPreferencePage.java
+++ src/main/java/it/albertus/routerlogger/gui/preference/page/ServerHttpsAdvancedPreferencePage.java
@@ -1,6 +1,7 @@
 package it.albertus.routerlogger.gui.preference.page;
 
 import java.net.HttpURLConnection;
+import java.util.Map.Entry;
 
 import it.albertus.jface.preference.StaticLabelsAndValues;
 import it.albertus.jface.preference.page.RestartHeaderPreferencePage;
@@ -9,9 +10,12 @@ import it.albertus.net.httpserver.AbstractHttpHandler;
 public class ServerHttpsAdvancedPreferencePage extends RestartHeaderPreferencePage {
 
 	public static StaticLabelsAndValues getRedirectStatusComboOptions() {
-		final StaticLabelsAndValues options = new StaticLabelsAndValues(2);
-		options.put(AbstractHttpHandler.getHttpStatusCodes().get(HttpURLConnection.HTTP_MOVED_PERM), HttpURLConnection.HTTP_MOVED_PERM);
-		options.put(AbstractHttpHandler.getHttpStatusCodes().get(HttpURLConnection.HTTP_MOVED_TEMP), HttpURLConnection.HTTP_MOVED_TEMP);
+		final StaticLabelsAndValues options = new StaticLabelsAndValues();
+		for (final Entry<Integer, String> entry : AbstractHttpHandler.getHttpStatusCodes().entrySet()) {
+			if (entry.getKey() >= HttpURLConnection.HTTP_MULT_CHOICE && entry.getKey() < HttpURLConnection.HTTP_BAD_REQUEST) {
+				options.put(entry.getKey() + " - " + entry.getValue(), entry.getKey());
+			}
+		}
 		return options;
 	}
 
