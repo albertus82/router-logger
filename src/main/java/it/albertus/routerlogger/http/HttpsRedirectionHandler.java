43d0bcf33e59841d909b401a067c9d200b8070a9
Alberto <albertus82@gmail.com>
2017-11-29 10:24:59 +0100

    HttpsRedirectionHandler: improved host header check.

--- src/main/java/it/albertus/routerlogger/http/HttpsRedirectionHandler.java
+++ src/main/java/it/albertus/routerlogger/http/HttpsRedirectionHandler.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 import com.sun.net.httpserver.HttpExchange;
 
 import it.albertus.net.httpserver.AbstractHttpHandler;
+import it.albertus.net.httpserver.HttpException;
 import it.albertus.net.httpserver.annotation.Path;
 import it.albertus.util.logging.LoggerFactory;
 
@@ -17,12 +18,18 @@ public class HttpsRedirectionHandler extends AbstractHttpHandler {
 
 	private static final int DEFAULT_HTTPS_PORT = 443;
 
+	private static final String HOST_HEADER_KEY = "Host";
+	private static final String LOCATION_HEADER_KEY = "Location";
+
 	private static final Logger logger = LoggerFactory.getLogger(HttpsRedirectionHandler.class);
 
 	private final RedirectionServerConfig config;
 
 	public HttpsRedirectionHandler(final RedirectionServerConfig config) {
 		super(config);
+		if (config == null) {
+			throw new NullPointerException("config cannot be null");
+		}
 		this.config = config;
 	}
 
@@ -30,8 +37,7 @@ public class HttpsRedirectionHandler extends AbstractHttpHandler {
 	protected void doGet(final HttpExchange exchange) throws IOException {
 		String host = config.getSslRedirectionLocationHost();
 		if (host == null || host.trim().isEmpty() || RedirectionServerConfig.DEFAULT_SSL_REDIRECTION_LOCATION_HOST.equals(host.trim())) {
-			host = exchange.getRequestHeaders().getFirst("Host").split(":")[0]; // discard the port number
-			logger.log(Level.CONFIG, "SSL host location not configured. Using the \"Host\" request header: \"{0}\".", host);
+			host = getHostFromRequest(exchange);
 		}
 
 		final int port = config.getSslRedirectionLocationPort();
@@ -60,10 +66,27 @@ public class HttpsRedirectionHandler extends AbstractHttpHandler {
 		}
 
 		final String location = sb.toString();
-		exchange.getResponseHeaders().set("Location", location);
+		exchange.getResponseHeaders().set(LOCATION_HEADER_KEY, location);
 		logger.log(Level.FINE, "Redirecting to: \"{0}\".", location);
 		exchange.sendResponseHeaders(responseCode, -1);
 		exchange.getResponseBody().close();
+	}
+
+	private static String getHostFromRequest(final HttpExchange exchange) {
+		if (!exchange.getRequestHeaders().containsKey(HOST_HEADER_KEY)) {
+			throw new HttpException(HttpURLConnection.HTTP_BAD_REQUEST, "Missing " + HOST_HEADER_KEY + " header.");
+		}
+		else {
+			final String hostHeaderValue = exchange.getRequestHeaders().getFirst(HOST_HEADER_KEY).trim();
+			if (hostHeaderValue.isEmpty()) {
+				throw new HttpException(HttpURLConnection.HTTP_BAD_REQUEST, "Empty " + HOST_HEADER_KEY + " header.");
+			}
+			else {
+				final String host = hostHeaderValue.split(":")[0].trim(); // discard the port number, if present
+				logger.log(Level.CONFIG, "SSL host location not configured. Using the \"Host\" request header: \"{0}\".", host);
+				return host;
+			}
+		}
 	}
 
 }
